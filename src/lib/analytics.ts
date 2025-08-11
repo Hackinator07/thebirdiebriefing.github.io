ffb662ffd1a9d3adbb7f3fb989c5e09bd6756061
Steve Luongo <luongosb@gmail.com>
2025-08-11 16:47:10 -0400

    Updated analytics.ts to use thebirdiebriefing

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -2,7 +2,7 @@ import Analytics from 'analytics'
 import googleAnalytics from '@analytics/google-analytics'
 
 export const analytics = Analytics({
-  app: 'golfgirlgazette',
+  app: 'thebirdiebriefing',
   plugins: [
     googleAnalytics({
       measurementIds: ['G-2N3SDEMB86']
